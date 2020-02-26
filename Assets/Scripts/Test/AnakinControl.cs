d966a6fd9d64bf6876d572fe8f60df7dba7db30c
unknown <andrewsknick@gmail.com>
2020-02-26 05:43:20 -0500

    Implemented Idle Slash control

--- Assets/Scripts/Test/AnakinControl.cs
+++ Assets/Scripts/Test/AnakinControl.cs
@@ -7,11 +7,18 @@ public class AnakinControl : MonoBehaviour
     public Animator a;
     public SpriteRenderer sr;
     private int JumpFrameCount;
+    private int SlashAFrameCount;
+    private int SlashBFrameCount;
+    private int SlashCFrameCount;
+
 
     // Start is called before the first frame update
     void Start()
     {
         JumpFrameCount = 0;
+        SlashAFrameCount = 0;
+        SlashBFrameCount = 0;
+        SlashCFrameCount = 0;
     }
 
     // Update is called once per frame
@@ -22,24 +29,12 @@ public class AnakinControl : MonoBehaviour
         bool mov = false;
         a.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
         a.SetFloat("Vertical", Input.GetAxis("Vertical"));
-        if (a.GetFloat("Horizontal") == 0 && a.GetFloat("Vertical") == 0)
+        if (a.GetFloat("Horizontal") < 0.5 && a.GetFloat("Horizontal") > -0.5
+            && a.GetFloat("Vertical") < 0.5 && a.GetFloat("Vertical") > -0.5)
             mov = false;
         else
             mov = true;
         a.SetBool("Idle", !mov);
-
-        //slash control
-        //triggered
-        if (Input.GetKeyDown("left ctrl"))
-        {
-            a.Play("Anakin Slash A");
-            a.SetBool("Slash A", true);
-        }
-        if (Input.GetKeyDown("left ctrl") && a.GetBool("Slash A"))
-        {
-            a.Play("Anakin Slash B");
-            a.SetBool("Slash A", false);
-        }
 
         //sprinting control
         //continuous
@@ -58,15 +53,17 @@ public class AnakinControl : MonoBehaviour
 
         //jump control
         //triggered
-        if (Input.GetKeyDown("space") && !a.GetBool("Jump") && JumpFrameCount == 0)
+        if (Input.GetKeyDown("space") && !a.GetBool("Jump") 
+            && !a.GetBool("Slash") && JumpFrameCount == 0)
         {
-            Debug.Log("I entered Jump control");
+            Debug.Log("Entered Jump control");
             a.SetBool("Jump", true);
             a.Play("Anakin Jump");
         }
         if (a.GetBool("Jump"))
         {
             JumpFrameCount++;
+            Debug.Log("Counted Jump Frame: " + JumpFrameCount);
             if (Input.GetKey("left shift"))
             {
                 a.SetBool("Sprint", false);
@@ -75,9 +72,91 @@ public class AnakinControl : MonoBehaviour
         }
         if (JumpFrameCount == 40)
         {
-            Debug.Log("I exited Jump control");
-            JumpFrameCount = 0;
+            Debug.Log("Exited Jump control");
             a.SetBool("Jump", false);
+            JumpFrameCount = 0;
+        }
+
+        //idle slash control
+        //triggered
+        //slash all
+        bool actSlash = (Input.GetKeyDown("left ctrl") && !a.GetBool("Jump") && a.GetBool("Idle"));
+
+        if (a.GetBool("Slash A") || a.GetBool("Slash B") || a.GetBool("Slash C"))
+            a.SetBool("Slash", true);
+        else
+            a.SetBool("Slash", false);
+
+        //slash A
+        if (actSlash && SlashAFrameCount == 0)
+        {
+            Debug.Log("Entering Slash A control");
+            a.Play("Anakin Slash A");
+            a.SetBool("Slash A", true);
+        }
+        if (a.GetBool("Slash A"))
+        {
+            Debug.Log("Counting Slash A frames: " + SlashAFrameCount);
+            mov = false;
+            SlashAFrameCount++;
+            transform.position = transform.position;
+        }
+        if(SlashAFrameCount >= 40)
+        {
+            SlashAFrameCount = 0;
+            Debug.Log("Exiting Slash A frame count");
+            a.SetBool("Slash A", false);
+        }
+
+        //slash B
+        if (actSlash && SlashAFrameCount >= 10 && SlashBFrameCount == 0)
+        {
+            Debug.Log("Entering Slash A to Slash B control");
+            a.Play("Anakin Slash B");
+            SlashAFrameCount = 0;
+            a.SetBool("Slash A", false);
+            a.SetBool("Slash B", true);
+        }
+        if (a.GetBool("Slash B"))
+        {
+            Debug.Log("Counting Slash B frames: " + SlashBFrameCount);
+            mov = false;
+            SlashBFrameCount++;
+            transform.position = transform.position;
+        }
+        if (SlashBFrameCount >= 25)
+        {
+            SlashAFrameCount = 0;
+            SlashBFrameCount = 0;
+            Debug.Log("Exiting Slash B frame count");
+            a.SetBool("Slash B", false);
+        }
+
+        //slash C or kick
+        if (actSlash && SlashBFrameCount >= 5 && SlashCFrameCount == 0)
+        {
+            Debug.Log("Entering Slash B to Slash C control");
+            a.Play("Anakin Slash Kick");
+            SlashBFrameCount = 0;
+            SlashCFrameCount = 0;
+            a.SetBool("Slash A", false);
+            a.SetBool("Slash B", false);
+            a.SetBool("Slash C", true);
+        }
+        if (a.GetBool("Slash C"))
+        {
+            Debug.Log("Counting Slash C/Kick frames: " + SlashCFrameCount);
+            mov = false;
+            SlashCFrameCount++;
+            transform.position = transform.position;
+        }
+        if (SlashCFrameCount >= 20)
+        {
+            SlashAFrameCount = 0;
+            SlashBFrameCount = 0;
+            SlashCFrameCount = 0;
+            Debug.Log("Exiting Slash C frame count");
+            a.SetBool("Slash C", false);
         }
 
         //diagonal movement control
@@ -87,17 +166,20 @@ public class AnakinControl : MonoBehaviour
             a.SetBool("Slant", false);
 
         //direction control
-        if (a.GetFloat("Horizontal") < 0)
-            sr.flipX = true;
-        if(a.GetFloat("Horizontal") > 0)
-            sr.flipX = false;
+        if(!a.GetBool("Slash"))
+        {
+            if (a.GetFloat("Horizontal") < 0)
+                sr.flipX = true;
+            if (a.GetFloat("Horizontal") > 0)
+                sr.flipX = false;
+        }
 
         //speed control
         Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
         Vector3 vertical = new Vector3(0.0f, Input.GetAxis("Vertical"), 0.0f);
-        if (!spr)
+        if (!spr && mov && !a.GetBool("Slash"))
             transform.position += (horizontal + vertical) * Time.deltaTime * 5;
-        else
+        else if (spr && !a.GetBool("Slash"))
             transform.position += (horizontal + vertical) * Time.deltaTime * 10;
 
     }
